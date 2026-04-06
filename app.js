7a4c7e5159e710acc5b13f730ee550bfb81de0de
Yoni190 <yfisuhu@gmail.com>
2026-04-06 15:25:02 +0300

    Seup session store

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 const express = require('express')
 const path = require('node:path')
 const authRoute = require('./routes/authRoute')
+const session = require('express-session')
+const passport = require('passport')
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store')
+
+import { prisma } from '../lib/prisma'
 require('dotenv').config()
 
 
 
-
-
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const sessionStore = new PrismaSessionStore(
+    prisma,
+    {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined
+    }
+)
+app.use(
+    session({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        },
+        secret: process.env.SECRET,
+        resave: true,
+        saveUninitialized: true,
+        store: sessionStore
+    })
+)
+app.use(Passport.session())
+app.use((req, res, next) => {
+    res.locals.user = req.user
+    next()
+})
+
+
 app.use('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
