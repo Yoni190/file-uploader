db5d69c4f9d74c436f06932ed78cf78cd218bf60
Yoni190 <yfisuhu@gmail.com>
2026-04-06 15:27:25 +0300

    Create user routes

---
+++ routes/userRoute.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+const userController = require('../controllers/userController')
+
+const router = Router()
+
+router.get('/register', userController.registerView)
+
+
+module.exports = router
