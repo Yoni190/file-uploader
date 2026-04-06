3988804e4964a11e4f6516d57fb97a0b056a16e3
Yoni190 <yfisuhu@gmail.com>
2026-04-06 15:07:17 +0300

    Create function to logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,3 +68,15 @@ exports.login = [
         failureRedirect: '/login'
     })
 ]
+
+exports.logout = [
+    (req, res, next) => {
+        req.logout((err) => {
+            if(err) {
+                return next(err)
+            }
+
+            res.redirect('/login')
+        })
+    }
+]
