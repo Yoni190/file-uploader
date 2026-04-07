23edd02b323d57d90afbe70e0f09b7eb5f8d0277
Yoni190 <yfisuhu@gmail.com>
2026-04-07 15:46:06 +0300

    Setup static files

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ app.use((req, res, next) => {
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.use(authRoute)
 app.use(userRoute)
