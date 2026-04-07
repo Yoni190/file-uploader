502ea43a63c04f843a830e5dcc5918f3e8d0801e
Yoni190 <yfisuhu@gmail.com>
2026-04-07 16:59:40 +0300

    Add a function to create folders

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,7 +1,25 @@
+const { prisma } = require('../lib/prisma')
+
 exports.createView = [
     (req, res) => {
         res.render('add_folder', {
             title: 'Create Folder'
         })
+    }
+]
+
+exports.create = [
+    async (req, res) => {
+        const { folder_name } = req.body
+        await prisma.folder.create({
+            data: {
+                name: folder_name,
+                user: {
+                    connect: { id: req.user.id }
+                }
+            }
+        })
+
+        return res.redirect('/')
     }
 ]
