7e31099b5ddc1b434e4261a4d277d61caf7056bc
Yoni190 <yfisuhu@gmail.com>
2026-04-08 15:02:51 +0300

    Create route to edit folders

--- routes/folderRoute.js
+++ routes/folderRoute.js
@@ -7,5 +7,6 @@ router.get('/create-folder', isAuth, folderController.createView)
 router.post('/create-folder', isAuth, folderController.create)
 router.get('/folder/:id', isAuth, folderController.folderDetails)
 router.get('/edit-folder/:id', isAuth, folderController.editView)
+router.post('/edit-folder/:id', isAuth, folderController.edit)
 
 module.exports = router
