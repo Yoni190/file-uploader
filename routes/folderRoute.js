009ba50599c86d30deb46fe48308303c72c83c2c
Yoni190 <yfisuhu@gmail.com>
2026-04-08 14:58:57 +0300

    Create route to get edit folder view

--- routes/folderRoute.js
+++ routes/folderRoute.js
@@ -6,5 +6,6 @@ const { isAuth } = require('../middleware/isAuth')
 router.get('/create-folder', isAuth, folderController.createView)
 router.post('/create-folder', isAuth, folderController.create)
 router.get('/folder/:id', isAuth, folderController.folderDetails)
+router.get('/edit-folder/:id', isAuth, folderController.editView)
 
 module.exports = router
