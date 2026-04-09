e7f6d5d95357763b944026087893a9c74f7170e6
Yoni190 <yfisuhu@gmail.com>
2026-04-09 11:04:20 +0300

    Store file details in the database

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,5 +1,20 @@
+const { prisma } = require('../lib/prisma')
+
 exports.upload = [
-    (req, res) => {
-        res.redirect('/')
+    async (req, res) => {
+        const folderId = parseInt(req.params.id)
+        const fileName = req.file.originalname
+        const size = req.file.size
+
+        await prisma.file.create({
+            data: {
+                name: fileName,
+                size: size,
+                folder: {
+                    connect: { id: folderId }
+                }
+            }
+        })
+        res.redirect(`/folder/${folderId}`)
     }
 ]
