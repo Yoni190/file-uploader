9d82fa58b57838d18242fc8f54c60910cc8ac32c
Yoni190 <yfisuhu@gmail.com>
2026-04-09 12:42:36 +0300

    Switch multer to memory storage

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -3,10 +3,11 @@ const router = Router()
 const fileController = require('../controllers/fileController')
 const { isAuth } = require('../middleware/isAuth')
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ storage: multer.memoryStorage() })
 
 
 router.post('/upload/:id', upload.single('file'), fileController.upload)
 router.post('/delete-file/:id', fileController.deleteFile)
+router.get('/download/:id', fileController.download)
 
 module.exports = router
