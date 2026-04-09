0434e1cb77cd5eb48932bda9f630675fb83a8bbd
Yoni190 <yfisuhu@gmail.com>
2026-04-09 11:45:08 +0300

    Upload file to supabase

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,10 +1,30 @@
 const { prisma } = require('../lib/prisma')
+const { createClient } = require('@supabase/supabase-js')
+require('dotenv').config()
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 exports.upload = [
     async (req, res) => {
+        const file = req.file
         const folderId = parseInt(req.params.id)
-        const fileName = req.file.originalname
-        const size = parseFloat((req.file.size / 1000000).toFixed(2))
+        const fileName = file.originalname
+        const size = parseFloat((file.size / 1000000).toFixed(2))
+
+        const {data, error } = await supabase.storage
+                      .from('files')
+                      .upload(`public/${fileName}`, file, {
+                        cacheControl: '3600',
+                        upsert: false
+                      })
+
+        if(error) {
+            console.log(error)
+        } else {
+            console.log(data)
+        }
+
+        
 
         await prisma.file.create({
             data: {
